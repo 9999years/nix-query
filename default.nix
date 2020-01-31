b99cecda7afcc3d49187ae70b51db030e6d8b8da
Rebecca Turner <rbt@sent.as>
2020-01-30 23:44:37 -0500

    bump nix build

--- default.nix
+++ default.nix
@@ -1,15 +1,14 @@
 { stdenv, lib, rustPlatform, fetchFromGitHub, ... }:
 rustPlatform.buildRustPackage rec {
   pname = "nix-query";
-  version = "0.1.2";
+  version = "1.0.2";
 
   src = fetchFromGitHub {
     owner = "9999years";
     repo = "nix-query";
     rev = "v${version}";
-
     sha512 =
-      "06q52an1my6208zcgplf2gw1xgm7v7x8qcisz4x39ckvxbl0rr1nj15b82qn256j92gc1bbir04v9d5xmmxm5a1rg60yg673i8nss5s";
+      "3yin1rmwggvfgy87qfhbndz8828x2g82csxlv41sibxa09nnblhfl5ig5zpln96lm2zjrck03bg1xmw3ihjkkbcix0zmbrd29502wrm";
   };
 
   cargoSha256 = "1wicg5709s0i8z72xiz3a7z6m8zv8y5g6i9jkdxrahqkw8fpyaj7";
